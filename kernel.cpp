af32ed5b1c1ac5e6d02829623137d30e72662782
Muttsuri <Muttsuri@outlook.com>
2017-06-06 17:02:54 +0100

    Add/Update Kernel.cpp
    This is the core of the OS.

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,16 @@
-void kernelMain()
+void printf(char* str) //we have to code a printf becuase as we are building an os we have no linker to add the library stdio.h which has the printf function.
+{
+    (unsigned short*) VideoMemory = (unsigned short*)0xb8000;
+    for(int i = 0; str[i] !='\0'; i++)
+    {
+        VideoMemory[i] = str[i];
+    }
+}
+
+void kernelMain(void* multiboot_structure, unsigned int magicnumber) //void pointer to accept the multiboot data passed from loader, also accepts the magic number
 {
     printf("I have booted");
+
+
+    while (1); //prevent kernel from stoping
 }
