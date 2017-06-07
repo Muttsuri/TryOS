73d98d27933ccc40ed8dc97308cca1fae10a2c2f
Muttsuri <Muttsuri@outlook.com>
2017-06-07 12:20:28 +0100

    Compleat kernel.cpp
    Fix bad syntax (unsigned short*) should be: unsigned short*

--- kernel.cpp
+++ kernel.cpp
@@ -1,16 +1,19 @@
 void printf(char* str) //we have to code a printf becuase as we are building an os we have no linker to add the library stdio.h which has the printf function.
 {
-    (unsigned short*) VideoMemory = (unsigned short*)0xb8000;
+    unsigned short* VideoMemory = (unsigned short*)0xb8000; //video memory starts at 0xb8000
     for(int i = 0; str[i] !='\0'; i++)
     {
-        VideoMemory[i] = str[i];
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; //This copies to video memory the value each character in the string
+                                                             //but it also copies the high byte of the video memory so that the characters remain white
+                                                             //this is the default of the video memory
+                                                             //after copying the high byte it combines it with the character to make the white charctet
+                                                             //high byte defines the colour of the character and the second defines the value to be presented.
     }
 }
-
-void kernelMain(void* multiboot_structure, unsigned int magicnumber) //void pointer to accept the multiboot data passed from loader, also accepts the magic number
+//extern "C" -> avoid C++ alternative naming conventions, use C naming conventions
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) //void pointer to accept the multiboot data passed from loader, also accepts the magic number
 {
     printf("I have booted");
-
-
+    
     while (1); //prevent kernel from stoping
 }
