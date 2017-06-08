f148a0672ad55d02a65c16255acbb9c3971a3fc8
Muttsuri <Muttsuri@outlook.com>
2017-06-08 17:24:21 +0100

    Add construtors and commentation
    Constructors are necessery for composite datatypes, however this
    may be a C++ thing and I'm not shure if I am going to swtich to C
    I belive most likely I won't. But just in case the note stays here.

--- kernel.cpp
+++ kernel.cpp
@@ -10,6 +10,28 @@ void printf(char* str) //we have to code a printf becuase as we are building an 
                                                              //high byte defines the colour of the character and the second defines the value to be presented.
     }
 }
+
+/*Constructor for global objects*/
+//At this level without the constructor we would run into problems with:
+// * Global Ojects
+// * Static Objects in classes
+// * Composite Objects (Class and Structure Instances)
+//If we would work with a pointes to those data types we would run into that problem
+//Becuse pointers are a primitive datatype
+
+//Tl;Dr This constructor allows more complex data types
+typedef void (*constructor)(); //define constructor as a pointer to a function
+extern "C" constructor start_ctors; //pointer to start_ctors data constructor 
+extern "C" constructor end_ctors;  //pointer to end_ctors data constructor
+
+extern "C" void CallConstructors() 
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    { (*i)(); }
+}
+/*Constructor for global objects*/
+
+
 //extern "C" -> avoid C++ alternative naming conventions, use C naming conventions
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) //void pointer to accept the multiboot data passed from loader, also accepts the magic number
 {
